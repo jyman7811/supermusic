a5cdf68ddd6e3a6f3ae1454b42267e1da428a122
jyman <51020575+jyman7811@users.noreply.github.com>
2020-04-27 00:39:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,18 @@ bot.load_extension("lyric")
 async def on_ready():
     await bot.change_presence(activity=discord.Streaming(name = '우리모두 "30초 손씻기" 실천합시다!', url='https://www.youtube.com/watch?v=P-rhqWpjVag'))
     print("봇 활성화")
+    
+@bot.command(aliases=["샤딩", "sharding", "shard"])
+async def shard_id(ctx):
+    await ctx.send(str(bot.latencies))
+    await ctx.send(str(bot.latencies[ctx.author.guild.shard_id][1]))
+    embed = discord.Embed(
+        title = "Sharding (샤딩)",
+        colour = discord.Colour.blurple()
+    )
+    embed.add_field(name="Shard_ID", value=str(ctx.author.guild.shard_id))
+    embed.add_field(name="Shard Latency", value=str(math.floor(round(bot.latencies[ctx.author.guild.shard_id][1]*1000))) + "ms")
+    await ctx.send(embed=embed)
 
 @bot.command(pass_context=True, aliases=["핑"])
 async def ping(ctx):
